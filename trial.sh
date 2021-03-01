eb3b70e4d7a4d613b671e2652dda64c31c1cef1e
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-03-02 01:58:20 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # Script auto create trial user SSH
 # yg akan expired setelah 1 hari
-# modified by White-vps.com
+# modified by SLSSH
 
 IP=`curl icanhazip.com`
 
@@ -12,34 +12,20 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "=====  Trial Premium Akun SSH =====" 
-echo -e "Host : $IP" 
+echo -e "=====  Trial 1 Hari Premium Akun SSH =====" 
+echo -e "Host SSH : #$IP" 
 echo -e "Port SSH" 
-echo -e "OpenSSH : 143,200,400,1078,8000" 
-echo -e "Dropbear : 44,77,450,550,9000" 
+echo -e "OpenSSH : 143,200,8000,40000" 
+echo -e "Dropbear : 44,77,9000,55000" 
 echo -e "SSL/TLS SSH : 443" 
-echo -e "SSL/TLS OpenSSH : 43,600,700,800,900" 
-echo -e "SSL/TLS Dropbear : 444,540,551,777,9900" 
-echo -e "SSL/TLS SSR SSH : 69" 
-echo -e "OpenVPN TLS/SSL : 569" 
+echo -e "SSL/TLS OpenSSH : 43,700,800,4000" 
+echo -e "SSL/TLS Dropbear : 444,777,9900,56000" 
 echo -e "BadVPN-UDPGW : 7100,7200,7300"
-echo -e "Speed Server : 2 Gbps" 
-echo -e "Transfer : 2 TB" 
+echo -e "Speed Server : 1 Gbps" 
 echo -e "Username : $Login " 
 echo -e "Password : $Pass" 
-echo -e "-----------------------------" 
+echo -e "---------------------------" 
 echo -e "Aktif Sampai      : $exp" 
-echo -e "Masa Aktif 1 Hari"
-echo -e "============================="
-echo -e "Mod by Sulaiman L" 
-echo -e "=============================" 
-echo -e "Config OpenVPN (TCP 56969): "
-echo -e "http:// $IP :81/client-tcp.ovpn" 
-echo -e "=============================" 
-echo -e "Config OpenVPN (UDP 1945): "
-echo -e "http:// $IP :81/client-udp.ovpn"
-echo -e "=============================" 
-echo -e "Config OpenVPN (TCP+SSL 569): "
-echo -e "http:// $IP :81/569client-ssl.ovpn
-echo -e "=============================" 
+echo -e "==========================="
+echo -e "Script Mod by Sulaiman L"  
 echo -e ""
